e4c9bb82fb2d9f6ff09fab71504fc747b3fa1bd4
PAzter1101 <PAzter1101@gmail.com>
2021-01-28 08:58:44 +0300

    -add doors.

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool _open;
+    void Start()
+    {
+        _open = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!_open)
+        {
+            if (collision.tag == "Player")
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
+                _open = true;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_open)
+        {
+            if (collision.tag == "Player")
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                _open = false;
+            }
+        }
+    }
+}
